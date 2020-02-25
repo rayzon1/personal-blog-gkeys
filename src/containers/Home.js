faa2a60da85b5711cb413bdb93e797ebbf50aeef
Gerardo Keys <jotojry@icloud.com>
2020-02-24 17:39:42 -0800

    initial commit, project setup, minor styles, react router added

---
+++ src/containers/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Home() {
+
+
+    return (
+        <div className="home">
+            <div className="home__section--1">
+                1
+            </div>
+            <div className="home__section--2">
+                2
+            </div>
+            <div className="home__section--3">
+                3
+            </div>
+        </div>
+    )
+}
