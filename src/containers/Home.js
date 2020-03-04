810a9a06c441ef68bea35be9c5bca0cd329f5b77
Gerardo Keys <jotojry@icloud.com>
2020-03-03 17:44:32 -0800

    multiple changes, transition animation added on nav

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,34 +1,36 @@
-import React, { createRef, useEffect, useState } from 'react';
-import Header from '../components/Header';
-import Navigation from '../components/Navigation';
-import NavigationList from '../components/NavigationList';
-import BlogPreview from '../components/BlogPreview';
-import ResourceList from '../components/ResourceList';
+import React, { useState } from "react";
+import Header from "../components/Header";
+import Navigation from "../components/Navigation";
+import NavigationList from "../components/NavigationList";
+import BlogPreview from "../components/BlogPreview";
+import ResourceList from "../components/ResourceList";
+import { CSSTransition } from "react-transition-group";
 
 export default function Home() {
-    const checkBoxRef = createRef();
-    const listRef = createRef();
-    const [isChecked, setIsChecked] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
 
-    //TODO: Create useState true/false to open or close nav
+  //TODO: Create useState true/false to open or close nav
 
-    
-
-    return (
-        <div className="home">
-            <div className="home__section--1">
-                <Navigation setIsChecked={setIsChecked}/>
-                {
-                    isChecked && <NavigationList/>
-                }
-            </div>
-            <div className="home__section--2">
-                <Header />
-                <div style={{ display: 'flex'}}>
-                    <BlogPreview />
-                    <ResourceList />
-                </div>
-            </div>
+  return (
+    <div className="home">
+      <div className="home__section--1">
+        <Navigation setIsChecked={setIsChecked} />
+        <CSSTransition
+          in={isChecked}
+          timeout={400}
+          classNames="nav"
+          unmountOnExit
+        >
+          <NavigationList />
+        </CSSTransition>
+      </div>
+      <div className="home__section--2">
+        <Header />
+        <div style={{ display: "flex" }}>
+          <BlogPreview />
+          <ResourceList />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
